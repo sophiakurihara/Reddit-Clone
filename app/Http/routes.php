03c0024d18c598f03abfb9f8eafd9d2d9634219e
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-24 11:31:58 -0500

    finished routing exercise

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/uppercase/{string?}', function($string = "string") {
+	$str = strtoupper($string);
+	return "$str";
+});
+
+Route::get('/increment/{int?}', function($int = 0) {
+	$inc = ++$int;
+	return $inc;
+});
+
+Route::get('/add/{num1?}/{num2?}', function($num1 = 0, $num2 = 1) {
+	$sum = $num1 + $num2;
+	return $sum;
+});
